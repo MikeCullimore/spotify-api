e6d8fd3d5301da8276fae60067e78dc9f664416b
Mike Cullimore <michael.cullimore11@gmail.com>
2023-05-09 21:36:38 +0100

    Call audio analysis endpoint.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+import { writeFileSync } from 'fs';
 import fetch from 'node-fetch';
 import { getClientId, getClientSecret } from "./environment.js";
+
+interface SpotifyTokenResponse {
+    access_token: string
+}
 
 const url = 'https://accounts.spotify.com/api/token';
 var authOptions = {
@@ -14,10 +19,35 @@ var authOptions = {
 };
 
 const response = await fetch(url, authOptions)
-const body = await response.text();
-const parsed = JSON.parse(body); // TODO: type guard, see https://stackoverflow.com/questions/38688822/how-to-parse-json-string-in-typescript
+const body = await response.json() as SpotifyTokenResponse; // TODO: type guard, see https://stackoverflow.com/questions/38688822/how-to-parse-json-string-in-typescript
 
-if (parsed?.access_token) {
-    const access_token = parsed?.access_token;
-    console.log(`Access token: ${access_token}`);
+// Analyse track.
+// https://open.spotify.com/track/49X0LAl6faAusYq02PRAY6?si=a7784942a7ef4a0a
+// TODO: get for all tracks in liked list.
+// TODO: link to track metadata (title, artist).
+
+const analyseAudio = async (accessToken: string, trackId: string) => {
+    const url = `https://api.spotify.com/v1/audio-analysis/${trackId}`;
+    const options = {
+        method: 'get',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+        },
+    }
+    const response = await fetch(url, options);
+    const body = await response.json();
+    // console.log(body);
+    // console.log(response.status);
+
+    writeFileSync("tracks/lady-hear-me-tonight.json", JSON.stringify(body, null, 2));
+}
+
+if (body?.access_token) {
+    const accessToken = body?.access_token;
+    // console.log(`Access token: ${accessToken}`);
+
+    const trackId = '49X0LAl6faAusYq02PRAY6?si=a7784942a7ef4a0a';
+    await analyseAudio(accessToken, trackId);
+} else {
+    throw Error("Invalid token response from Spotify API.");
 }
